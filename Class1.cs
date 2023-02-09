c38b6fed8050befc21f493c16027519b21b7109d
таранаа <@101-12>
2023-02-09 11:21:45 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiesSchoolLearn
+{
+    public partial class Service
+    {
+        public string PriceAndTime 
+        { get
+            {
+                //return Cost.ToString() + " рублей за " + (DurationInSeconds / 60).ToString() + " минут";
+                return ((double)Cost - (double)Cost *Discount).ToString() + " рублей за " + (DurationInSeconds / 60).ToString() + " минут";
+            }
+        }
+        public string DiscountIntext
+        {
+            get
+            {
+                if (Discount != 0)
+                {
+                    return "* скидка " + (Discount * 100).ToString() + "%";
+                }
+                return "";
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiesSchoolLearn
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DemoExam_TaranEntities demoExam_TaranEntities = new DemoExam_TaranEntities();
+            //dgTable.ItemsSource = demoExam_TaranEntities.Service.ToList();
+            servicelist.ItemsSource = demoExam_TaranEntities.Service.ToList();
+        }
+
+        private void dgTable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
